94c273e4dbf37102646f9a237f982085ffdb0a6f
Gurpreet Paul <gurpreet@gurpreet.co>
2018-05-10 00:15:00 +0100

    Now submodule'd java repo

--- app/src/main/java/com/goterl/lazycode/lazysodium/SodiumAndroid.java
+++ app/src/main/java/com/goterl/lazycode/lazysodium/SodiumAndroid.java
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) Terl Tech Ltd • 09/05/18 18:38 • goterl.com
+ * Copyright (c) Terl Tech Ltd • 09/05/18 23:08 • goterl.com
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-include ':lazysodium-java'
-include ':app'
+package com.goterl.lazycode.lazysodium;
 
+public class SodiumAndroid {
+}
