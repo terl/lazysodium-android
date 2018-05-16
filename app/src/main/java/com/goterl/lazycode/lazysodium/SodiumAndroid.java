d213cfad35408438f94736703c2e15969c69ef60
Gurpreet Paul <gurpreet@gurpreet.co>
2018-05-16 11:55:33 +0100

    Excluded NativeUtils and SodiumJava as they were primarily Java based classes

--- app/src/main/java/com/goterl/lazycode/lazysodium/SodiumAndroid.java
+++ app/src/main/java/com/goterl/lazycode/lazysodium/SodiumAndroid.java
@@ -8,5 +8,35 @@
 
 package com.goterl.lazycode.lazysodium;
 
-public class SodiumAndroid {
+import com.sun.jna.Native;
+
+public class SodiumAndroid extends com.goterl.lazycode.lazysodium.Sodium {
+
+
+    public SodiumAndroid() {
+        this("sodium");
+    }
+
+    /**
+     * If on the Android platform, then this is the intended
+     * route to load a libsodium.so. Please note loading for Android is not
+     * the same as loading for Java.
+     * The path must be to a directory with all the Android ABIs, which each
+     * contain the libsodium.so. For example, given {@code /path/to/ABI1/}, {@code /path/to/THE_ABI2/}
+     * and {@code /path/to/THE_ABI3/}, the param {@code path} should be {@code /path/to/}.
+     * ABI1, ABI2 and ABI3 all contain libsodium.so built for that specific ABI.
+     *
+     * @param path Absolute path to the parent directory of all the ABI directories.
+     */
+    public SodiumAndroid(String path) {
+
+        // If it's the android platform then we load
+        // the libsodium.so files from the "src/main/libs" folder.
+        // This folder should have folders with ABI names
+        // such as x86 or x86_64 etc.
+        Native.register(SodiumAndroid.class, path);
+
+    }
+
+
 }
