24290430c73e9057e3cebfb88cb817c31c2e0aa7
Gurpreet Paul <gurpreet@terl.co>
2018-07-24 16:25:46 +0100

    LazysodiumAndroid now pulls in tests from LazysodiumJava
    Of course, Gradle performs some magic to ensure that we copy the right tests and we can also delete the copied in tests too

---
+++ app/src/androidTest/java/com/goterl/lazycode/lazysodium/BaseTest.java
@@ -0,0 +1,15 @@
+package com.goterl.lazycode.lazysodium;
+/*
+ * Copyright (c) Terl Tech Ltd • 24/07/18 16:08 • goterl.com
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+
+public class BaseTest {
+
+    public static LazySodiumAndroid lazySodium = new LazySodiumAndroid(new SodiumAndroid());
+
+}
