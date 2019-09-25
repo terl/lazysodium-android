1845233a251111adc3ab63a7a1c2f259a7ee7828
Gurpreet Paul <gurpreet@terl.co>
2019-09-25 20:14:30 +0100

    Stop copying tests and now finalise them

--- app/src/androidTest/java/com/goterl/lazycode/lazysodium/BaseTest.java
+++ app/src/androidTest/java/com/goterl/lazycode/lazysodium/BaseTest.java
@@ -1,18 +1,22 @@
-package com.goterl.lazycode.lazysodium;
 /*
- * Copyright (c) Terl Tech Ltd • 24/07/18 16:08 • goterl.com
+ * Copyright (c) Terl Tech Ltd • 14/06/19 17:54 • goterl.com
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+package com.goterl.lazycode.lazysodium;
 
-import com.goterl.lazycode.lazysodium.LazySodiumAndroid;
-import com.goterl.lazycode.lazysodium.SodiumAndroid;
+import org.junit.Before;
 
 public class BaseTest {
 
-    public static LazySodiumAndroid lazySodium = new LazySodiumAndroid(new SodiumAndroid());
+    public static LazySodiumAndroid lazySodium;
+
+    @Before
+    public void doBeforeEverything() {
+        lazySodium = new LazySodiumAndroid(new SodiumAndroid());
+    }
 
 }
