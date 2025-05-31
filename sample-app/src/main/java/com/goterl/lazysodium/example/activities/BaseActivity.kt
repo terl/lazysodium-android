76e18548a0af285215f536c0d6d17369ac598311
Gurpreet Paul <gurpreet@gurpreet.co>
2025-05-31 01:57:05 +0100

    Fix sample app

--- sample-app/src/main/java/com/goterl/lazysodium/example/activities/BaseActivity.kt
+++ sample-app/src/main/java/com/goterl/lazysodium/example/activities/BaseActivity.kt
@@ -2,9 +2,15 @@ package com.goterl.lazysodium.example.activities
 
 import android.graphics.PorterDuff
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import com.goterl.lazysodium.LazySodiumAndroid
 import com.goterl.lazysodium.SodiumAndroid
 import com.goterl.lazysodium.example.R
@@ -13,6 +19,7 @@ abstract class BaseActivity : AppCompatActivity() {
     protected lateinit var ls: LazySodiumAndroid
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_symmetric_enc)
 
@@ -20,6 +27,24 @@ abstract class BaseActivity : AppCompatActivity() {
         // initialised once somewhere. Perhaps in an
         // Application.java class.
         ls = LazySodiumAndroid(SodiumAndroid())
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.displayCutout())
+            // Apply the insets as a margin to the view. This solution sets
+            // only the bottom, left, and right dimensions, but you can apply whichever
+            // insets are appropriate to your layout. You can also update the view padding
+            // if that's more appropriate.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                bottomMargin = insets.bottom
+                topMargin = insets.top
+                rightMargin = insets.right
+            }
+
+            // Return CONSUMED if you don't want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
     }
 
     protected fun setupToolbar(title: String?) {
